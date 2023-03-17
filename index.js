85ac02d90f283062d315a8e4288602dc7b187a41
Zak <Zaksmith04@gmail.com>
2023-03-16 22:23:59 -0400

    feature: refactor Player class, add gravity

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-
     const canvas = document.querySelector('canvas')
     const c = canvas.getContext('2d')
 
     canvas.width = 64*16
     canvas.height = 64*9
 
-    c.fillStyle = 'white';
-    c.fillRect(0,0,canvas.width, canvas.height)
+    
 
-    c.fillStyle = 'red';
-    c.fillRect(100,100,100,100)
+    const player = new Player()
+  
+
+ 
+    //let bottom = y+100
+
 
     function animate(){
         window.requestAnimationFrame(animate)
+        c.fillStyle = 'white';
+        c.fillRect(0,0,canvas.width, canvas.height)
+
+        player.draw()
+        player.update()
+      
+      
     }
 
     animate()
