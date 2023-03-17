e04c527300d521d93991d275d89dacdda1a60169
Zak <Zaksmith04@gmail.com>
2023-03-16 22:51:30 -0400

    feature: player movement, refactor event listeners

--- index.js
+++ index.js
@@ -6,18 +6,43 @@
 
     
 
+
+
+const backgroundLevel1 = new Sprite({
+    position:{
+        x:0,
+        y:0,
+    },
+    imageSrc:'./img/backgroundLevel1.png'
+})
+
     const player = new Player()
   
 
  
-    //let bottom = y+100
+ const keys = {
+    w:{
+        pressed:false
+    },
+    a:{
+        pressed:false
+    },
+    d:{
+        pressed:false
+    }
+ }
 
 
     function animate(){
         window.requestAnimationFrame(animate)
-        c.fillStyle = 'white';
-        c.fillRect(0,0,canvas.width, canvas.height)
+      
+        backgroundLevel1.draw()
 
+
+        player.velocity.x=0
+        if(keys.d.pressed)player.velocity.x=5
+        else if (keys.a.pressed) player.velocity.x=-5
+        
         player.draw()
         player.update()
       
@@ -25,3 +50,5 @@
     }
 
     animate()
+
+    
